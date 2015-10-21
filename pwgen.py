136439086fe95c4e370646ddf0da72d34cb34d91
Martin Unzner <martin.unzner@citrix.com>
2015-10-21 11:10:52 +0200

    Updated pwgen.py with a more sophisticated generator

--- pwgen.py
+++ pwgen.py
@@ -1,13 +1,25 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
+from random import randrange, choice, sample
 
-from random import choice
+# no y/z WRT American keyboard layout
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWX"
+lowercase_letters = "abcdefghijklmnopqrstuvwx"
+numbers = "1234567890"
+special_chars = ".,"
+alphanumerical = "".join([uppercase_letters, lowercase_letters, numbers])
+charcount = 10
 
-allowed_chars =\
-"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\
-1234567890+-*/.,_:;!?"
+def generate_password():
+    special_char_pos = randrange(charcount - 3) + 1
+    pwstr = choice(uppercase_letters)
+    pwstr += "".join(sample(alphanumerical, special_char_pos - 1))
+    pwstr += choice(special_chars)
+    pwstr += choice(lowercase_letters)
+    pwstr += "".join(sample(alphanumerical, charcount - special_char_pos - 3))
+    pwstr += choice(numbers)
+    return pwstr
 
-pwstr = ""
-for i in range(20):
-    pwstr += choice(allowed_chars)
-print(pwstr)
+if __name__ == "__main__":
+    for i in range(15):
+        print(generate_password())
 
