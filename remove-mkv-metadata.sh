bd87d80a895761b89cc200505bbd3f905683fffc
Martin Unzner <martin.u@posteo.de>
2021-12-21 21:03:44 +0100

    remove unused scripts

--- remove-mkv-metadata.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-ffmpeg -i "${1}" -map_metadata -1 -c:v copy -c:a copy "${1%.mkv}.mp4"
--- router-web-interface.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ssh -N -L 8082:localhost:80 root@skaro.lan &
-while ! curl -s -o /dev/null localhost:8082; do sleep 1; done
-firefox http://localhost:8082/
-read -r
