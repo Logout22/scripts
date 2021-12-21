50d8e2fed82e3760f1e978bf2e96641a2437742f
Martin Unzner <martin.u@posteo.de>
2021-12-21 21:46:45 +0100

    add download script for multi-language streams

---
+++ dl-orig-language.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -o errexit
+set -o nounset
+
+SOURCE=$1
+TARGET=$2
+# TODO adapt channel info before download
+youtube-dl -f 'hls-2670-0' -o "$TARGET-video.tmp" "$1"
+youtube-dl -f 'hls-aac-Originalton_englisch-0' -o "$TARGET-audio.tmp" "$1"
+ffmpeg -i "$TARGET-video.tmp" -i "$TARGET-audio.tmp" -map 0:0 -map 1:0 -map 0:1 -c:v copy -c:a copy "$TARGET.mkv"
