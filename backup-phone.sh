f7a936fee9029716520b838f8c3f59a86e88d9fe
Martin Unzner <martin.u@posteo.de>
2024-04-13 12:56:32 +0200

    add time tracking backup

--- backup-phone.sh
+++ backup-phone.sh
@@ -2,7 +2,7 @@
 set -eu
 
 cd "$HOME/Handy Backup"
-rsync-confirmed -e 'ssh -p 2222' --exclude .thumbnails user@OnePlus-8-Pro:/sdcard/{DCIM,Download,Movies,Pictures} .
+rsync-confirmed -e 'ssh -p 2222' --exclude .thumbnails user@OnePlus-8-Pro:/sdcard/{DCIM,Download,Movies,Pictures,Track\ Time} .
 echo Backing up WhatsApp folder
 ssh -p 2222 user@OnePlus-8-Pro 'tar -c -C /sdcard/Android/media/com.whatsapp WhatsApp' | pigz > Android/media/com.whatsapp/WhatsApp.tar.gz
 
