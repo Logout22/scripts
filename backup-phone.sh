aa0696877f54a19c9b2bcdfb88d6dd55b0b4deb8
Martin Unzner <martin.u@posteo.de>
2024-04-13 12:56:46 +0200

    remove local WhatsApp backup
    local backup seems to be no longer supported by WhatApp

--- backup-phone.sh
+++ backup-phone.sh
@@ -3,6 +3,4 @@ set -eu
 
 cd "$HOME/Handy Backup"
 rsync-confirmed -e 'ssh -p 2222' --exclude .thumbnails user@OnePlus-8-Pro:/sdcard/{DCIM,Download,Movies,Pictures,Track\ Time} .
-echo Backing up WhatsApp folder
-ssh -p 2222 user@OnePlus-8-Pro 'tar -c -C /sdcard/Android/media/com.whatsapp WhatsApp' | pigz > Android/media/com.whatsapp/WhatsApp.tar.gz
 
